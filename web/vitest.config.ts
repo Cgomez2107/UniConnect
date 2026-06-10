77482cd23495c2ff2a00d6f20f7c703187e18294
Juan Sebastian Martinez Jimenez <sebastiano805@hotmail.com>
2026-06-10 12:33:00 -0500

    fix(vitest): fix vitests

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -1,19 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  appType: 'spa',
   plugins: [react()],
-  server: {
-    host: 'localhost',
-    port: 8081,
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
     dedupe: ['react', 'react-dom'],
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
   },
 })
